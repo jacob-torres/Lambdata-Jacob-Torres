4e192da45c257542207b57974f22578cc9f64d62
= <=>
2021-01-17 01:46:37 -0700

    Added list_to_column function to helpful_functions module.

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from helpful_functions import tt_split, null_count, list_to_series
+
--- helpful_functions.py
+++ helpful_functions.py
@@ -3,15 +3,20 @@
     Provides basic data science functionality
 """
 
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 def tt_split(df, frac):
+
+
     """
         Returns the training and testing dataframes,
         where frac = fraction of df set aside for testing
     """
+
 
     df = df.copy()
     train, test = train_test_split(df, train_size=frac)
@@ -20,9 +25,12 @@ def tt_split(df, frac):
 
 
 def null_count(df):
+
+
     """
         Returns the sum of nulls in a dataframe
     """
+
 
     df = df.copy()
     sum = 0
@@ -32,3 +40,20 @@ def null_count(df):
 
     return sum
 
+
+def list_to_series(list, df, column_name):
+
+
+    """
+        Transforms a python list/array into a pandas series,
+        then returns the dataframe with the new series as a column.
+    """
+
+
+    df = df.copy()
+    series = pd.Series(list)
+    series.name = column_name
+    new_df = df.join(series)
+
+    return new_df
+    
