450a0795100c70601012b58e2b6e3a54e75d4fc6
= <=>
2021-01-21 09:25:07 -0700

    Increased to version 0.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='Lambdata-Jacob-Torres',
-    version='0.0.1',
+    version='0.0.2',
     author='Jacob Torres',
     author_email='jacob@jacobtorres.net',
     description='A simple python package for manipulating Pandas dataframes.',
@@ -18,5 +18,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
