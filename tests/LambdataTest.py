4aa2589af833d13c24e458ada08421a994138609
= <=>
2021-01-21 19:33:24 -0700

    Added skeleton of LambdataTest unittest

---
+++ tests/LambdataTest.py
@@ -0,0 +1,21 @@
+"""
+This is a unit test module for the functionality of the Lambdata class methods.
+"""
+
+import unittest
+import pandas as pd
+import lambdata/Lambdata
+
+class LambdataTest(unittest.TestCase):
+
+    def test_tt_split():
+        """Test the tt_split method."""
+        pass
+
+    def test_null_count():
+        """Test the null_count method."""
+        pass
+
+    def test_list_to_column():
+        """Test the list_to_column method."""
+        pass
